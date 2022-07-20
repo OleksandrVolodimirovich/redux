c19cc57ae265cf8324e61767447c242c206d2061
unknown <net_box@ukr.net>
2022-07-20 13:01:15 +0300

    [LES]зміна теми сторінки

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
 const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
 
 let state = 0;
 
@@ -25,5 +26,9 @@ asyncBtn.addEventListener('click', () => {
     render();
   }, 2000);
 });
+
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
 
 render();
